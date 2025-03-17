13b9bfb48392070454026523107391572a75e0fb
Mikel <mikelgartzia21@gmail.com>
2025-03-17 23:35:37 +0100

    [NEW] probando nueva ruta yv

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ComunidadController;
 
 Route::get('/', [ComunidadController::class, 'index']); 
-Route::get('/comunidad/{id}', [ComunidadController::class, 'show'])->name('comunidad.show');
+Route::get('/comunidad/{id}', [ComunidadController::class, 'showPanel'])->name('comunidad.show');
+Route::get('/enneo-tv/{poblacion}/{contador}-{instalacion}', [EnneoController::class, 'showPanel'])
+    ->name('presentacion.enneotv');
+
+// Route::get('/', [ComunidadController::class, 'getInstalacionesProyecto'])->name('presentacion.enneotv');
 
 // Otras rutas API para obtener los datos de proyectos, contadores y lecturas
 Route::get('proyectos-contadores', [ComunidadController::class, 'getProyectosConContadores']);
